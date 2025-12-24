336bb4a833a57afa8edc87223f0e6f9ac021abd6
Daniel Haramati <danielhar2010@gmail.com>
2025-12-24 23:17:08 +0200

    Add files via upload
    my try of a very basic alarm clock. 
    * will upgrade it btw

---
+++ Alarm_Clock.py
@@ -0,0 +1,32 @@
+import pygame
+import time
+from datetime import datetime, timedelta
+
+pygame.mixer.init() 
+pygame.mixer.music.load(r"C:\Users\Daniel\Music\calculator_project_music\elevator music.mp3")
+
+def alarm_clock(alarm_time_str):
+    alarm_time = datetime.strptime(alarm_time_str, "%H:%M").time() 
+    
+    now = datetime.now()
+    alarm_datetime = datetime.combine(now.date(), alarm_time)
+
+    if alarm_datetime <= now:
+        alarm_datetime += timedelta(days=1)
+    
+    print(f"Alarm set for: {alarm_datetime}")
+    while True:
+        current_time = datetime.now()
+
+        if current_time >= alarm_datetime:
+            print("WAKE UP!")
+            pygame.mixer.music.play()
+            
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+            break 
+        
+        time.sleep(1)
+
+# Run the alarm
+alarm_clock("7:30")
